92b319048adbd804fc242d74c982c2570cd2d074
theprogmatheus <theprog.matheus@gmail.com>
2022-06-10 16:11:27 -0300

    `AuthScreen` update

--- src/components/AuthScreen.js
+++ src/components/AuthScreen.js
@@ -1,15 +1,41 @@
 // import styles
 import './AuthScreen.css'
 
+import logo from './../assets/img/logo.png'
+
+
+import InputMask from 'react-input-mask';
+
 const AuthScreen = () => {
   return (
+
     <div className="auth-screen">
-      <form className="auth-screen-form" onSubmit={(e)=>{e.preventDefault()}}>
-        <input id="username" name="username" type="text" placeholder='Seu nome completo' />
-        <input id="phone" name="phone" type="text" placeholder='(XX) 9 XXXX-XXXX' />
+      <form className="auth-screen-form" onSubmit={(e) => { e.preventDefault() }}>
+
+        <div className="auth-screen-brand">
+          <img
+            className="auth-screen-logo"
+            src={logo}
+          />
+
+          <h1>I Want Eat</h1>
+        </div>
+
+        <input id="username" name="username" type="text" placeholder='Informe seu nome' autoComplete='off' />
+
+        <InputMask
+          id="phone"
+          name="phone"
+          mask='(99) 99999-9999'
+          placeholder='(00) 00000-0000'
+          type="tel"
+          maskChar="0"
+        />
+
         <input id="submit" type="submit" value='Entrar' />
       </form>
     </div>
+
   )
 }
 
